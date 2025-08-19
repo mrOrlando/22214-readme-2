b815d2e80e0f84ac4020bcd5af003e3f712d0a3e
Yury Karaliuk <juriy88@mail.com>
2025-08-19 15:39:59 +0300

    Add user authentication error handling: introduce AUTH_USER_NOT_FOUND and AUTH_USER_PASSWORD_WRONG constants, and implement verifyUser method in AuthService for user login validation.

--- project/apps/user/src/app/auth/auth.service.ts
+++ project/apps/user/src/app/auth/auth.service.ts
@@ -1,8 +1,17 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserRepository } from '../user/user.repository';
 import { UserEntity } from '../user/user.entity';
-import { CreateUserDto } from './dto';
-import { AUTH_USER_EXISTS_ERROR } from './auth.constants';
+import { CreateUserDto, LoginUserDto } from './dto';
+import {
+  AUTH_USER_EXISTS_ERROR,
+  AUTH_USER_NOT_FOUND,
+  AUTH_USER_PASSWORD_WRONG,
+} from './auth.constants';
 
 @Injectable()
 export class AuthService {
@@ -22,5 +31,26 @@ export class AuthService {
     }).setPassword(dto.password);
 
     return this.userRepository.save(user);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const { email, password } = dto;
+
+    const existingUser = await this.userRepository.findByEmail(email);
+
+    if (!existingUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    const userEntity = await new UserEntity(existingUser);
+    if (!(await userEntity.comparePassword(password))) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return userEntity.toPOJO();
+  }
+
+  public async getUser(id: string) {
+    return this.userRepository.findById(id);
   }
 }
