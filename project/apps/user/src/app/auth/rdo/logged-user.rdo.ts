e5ef806f01b6c55fd1109cf172d7d334fd2693b1
Yury Karaliuk <juriy88@mail.com>
2025-08-19 15:46:54 +0300

    Add RDO classes for user authentication: LoggedUserRdo and UserRdo to represent user data transfer objects.

---
+++ project/apps/user/src/app/auth/rdo/logged-user.rdo.ts
@@ -0,0 +1,7 @@
+export class LoggedUserRdo {
+  public id!: string;
+
+  public email!: string;
+
+  public accessToken!: string;
+}
---
+++ project/apps/user/src/app/auth/rdo/user.rdo.ts
@@ -0,0 +1,7 @@
+export class UserRdo {
+  public id!: string;
+
+  public name!: string;
+
+  public email!: string;
+}
