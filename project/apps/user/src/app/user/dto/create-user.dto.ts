1be19832738290e6f174ea2cfe94ad827b6bf4a4
Yury Karaliuk <juriy88@mail.com>
2026-02-04 12:42:35 +0300

    Remove user registration functionality and associated DTOs from user module, simplifying the UserController and HTTP documentation.

--- project/apps/user/src/app/user/dto/create-user.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateUserDto {
-  public name!: string;
-  public email!: string;
-  public password!: string;
-}
--- project/apps/user/src/app/user/user.controller.ts
+++ project/apps/user/src/app/user/user.controller.ts
@@ -1,20 +1,13 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserModel } from './user.schema';
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller({ path: 'users' })
 export class UserController {
   constructor(
     @InjectModel(UserModel.name) private userModel: Model<UserModel>
   ) {}
-
-  @Post('/register')
-  public create(@Body() createUserDto: CreateUserDto): Promise<UserModel> {
-    const newUser = new this.userModel(createUserDto);
-    return newUser.save();
-  }
 
   @Get('/')
   public async index(): Promise<UserModel[]> {
