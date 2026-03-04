f0870d81bf0c3eb74a824bcadaaddae8869090b0
Yury Karaliuk <juriy88@mail.com>
2026-03-04 12:23:24 +0300

    Add BasePostgresRepository class for PostgreSQL integration and update index exports

--- project/libs/shared/helpers/src/index.ts
+++ project/libs/shared/helpers/src/index.ts
@@ -2,5 +2,6 @@ export * from './repository/entity.interface';
 export * from './repository/repository.interface';
 export * from './repository/base-memory.repository';
 export * from './repository/base-mongo.repository';
+export * from './repository/base-postgres.repository';
 export * from './fillDto';
 export * from './getMongoConnectionString';
